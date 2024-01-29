fc04b1af3dbbc6b2e70204eed75277d198c79450
Vika <vikapetrova8@gmail.com>
2024-01-29 22:52:14 +0300

    another way

--- src/components/Rating.tsx
+++ src/components/Rating.tsx
@@ -10,21 +10,20 @@ export const Rating = (props: RatingPropsType) => {
 
     return (
         <div>
-            <Star selected={value > 0} setValue={setValue} value={1}/>
-            <Star selected={value > 1} setValue={setValue} value={2}/>
-            <Star selected={value > 2} setValue={setValue} value={3}/>
-            <Star selected={value > 3} setValue={setValue} value={4}/>
-            <Star selected={value > 4} setValue={setValue} value={5}/>
+            <Star selected={value > 0} setValue={() => setValue(1)}/>
+            <Star selected={value > 1} setValue={() => setValue(2)}/>
+            <Star selected={value > 2} setValue={() => setValue(3)}/>
+            <Star selected={value > 3} setValue={() => setValue(4)}/>
+            <Star selected={value > 4} setValue={() => setValue(5)}/>
         </div>
     );
 };
 
 type StarPropsType = {
     selected: boolean
-    value: number
-    setValue: (n: number) => void
+    setValue: () => void
 }
 
 const Star = (props: StarPropsType) => {
-    return <span onClick={() => props.setValue(props.value)}> {props.selected ? <b>star</b> : 'star'} </span>
+    return <span onClick={() => props.setValue()}> {props.selected ? <b>star</b> : 'star'} </span>
 }
