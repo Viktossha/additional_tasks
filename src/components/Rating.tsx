ec2a6f0f31407685e3cb12dd42b7601576aef60d
Vika <vikapetrova8@gmail.com>
2024-01-29 22:17:32 +0300

    rating fix

--- src/components/Rating.tsx
+++ src/components/Rating.tsx
@@ -8,33 +8,23 @@ export const Rating = (props: RatingPropsType) => {
 
     const [value, setValue] = useState(0)
 
-    const onClickHandler = () => {
-        setValue(n)
-    }
-
     return (
         <div>
-            <Star selected={value > 0} onClickHandler={onClickHandler}/>
-            <Star selected={value > 1}/><button onClick={() => {setValue(2)}}>2</button>
-            <Star selected={value > 2}/><button onClick={() => {setValue(3)}}>3</button>
-            <Star selected={value > 3}/><button onClick={() => {setValue(4)}}>4</button>
-            <Star selected={value > 4}/><button onClick={() => {setValue(5)}}>5</button>
+            <Star selected={value > 0} setValue={setValue} value={1}/>
+            <Star selected={value > 1} setValue={setValue} value={2}/>
+            <Star selected={value > 2} setValue={setValue} value={3}/>
+            <Star selected={value > 3} setValue={setValue} value={4}/>
+            <Star selected={value > 4} setValue={setValue} value={5}/>
         </div>
     );
 };
 
 type StarPropsType = {
     selected: boolean
-    onClickHandler: (n: number) => void
+    value: number
+    setValue: (n: number) => void
 }
 
 const Star = (props: StarPropsType) => {
-
-    if (props.selected) {
-        return (
-            <span onClick={props.onClickHandler}><b>star</b></span>
-        )
-    } else {
-        return <span>star</span>
-    }
+    return <span onClick={() => props.setValue(props.value)}> {props.selected ? <b>star</b> : 'star'} </span>
 }
