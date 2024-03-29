029db7a4304d2e60686ef045138a32d6e0ffb19a
Vika <vikapetrova8@gmail.com>
2024-03-30 00:05:46 +0300

    UncontrolledRating stories

---
+++ src/components/UncontrolledRating/UncontrolledRating.stories.tsx
@@ -0,0 +1,9 @@
+import {UncontrolledRating} from "./UncontrolledRating";
+
+export default {
+    component: UncontrolledRating
+}
+
+export const RatingChanging = () => {
+    return <UncontrolledRating/>
+}
