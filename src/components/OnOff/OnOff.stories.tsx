96075fc333d58d1362620d02cd83716fce9fe649
Vika <vikapetrova8@gmail.com>
2024-03-29 23:41:39 +0300

    OnOff stories

--- src/components/OnOff/OnOff.stories.tsx
+++ src/components/OnOff/OnOff.stories.tsx
@@ -1,16 +1,19 @@
 import {OnOff} from "./OnOff";
 import {useState} from "react";
+import {action} from "@storybook/addon-actions";
 
 export default {
     component: OnOff
 }
 
+const onClickHandler = action('on or off clicked')
+
 export const OnMode = () => {
-    return <OnOff status={true} onClick={() => {}}/>
+    return <OnOff status={true} onClick={onClickHandler}/>
 }
 
 export const OffMode = () => {
-    return <OnOff status={false} onClick={() => {}}/>
+    return <OnOff status={false} onClick={onClickHandler}/>
 }
 
 export const ModeChanging = () => {
