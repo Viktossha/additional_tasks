1b8d56bbb8c2cf074b816d922c61c91fc03cadaa
Vika <vikapetrova8@gmail.com>
2024-03-31 22:02:21 +0300

    ControlledInput stories

--- src/stories/Input.stories.tsx
+++ src/stories/Input.stories.tsx
@@ -39,4 +39,29 @@ export const GetValueOfUncontrolledInputByButtonPress = () => {
   )
 }
 
-export const ControlledInput = () => <input value={'fixed value'}/>
+export const ControlledInput = () => {
+  const [parentValue, setParentValue] = useState('')
+  const onChange = (e: ChangeEvent<HTMLInputElement>) => setParentValue(e.currentTarget.value)
+
+  return <input value={parentValue} onChange={onChange}/>
+}
+
+export const ControlledCheckbox = () => {
+  const [parentValue, setParentValue] = useState(true)
+  const onChange = (e: ChangeEvent<HTMLInputElement>) => {setParentValue(e.currentTarget.checked)}
+  return <input type="checkbox" checked={parentValue} onChange={onChange}/>
+}
+
+export const ControlledSelect = () => {
+  const [parentValue, setParentValue] = useState<string | undefined>('2')
+  const onChange = (e: ChangeEvent<HTMLSelectElement>) => {setParentValue(e.currentTarget.value)}
+
+  return <select value={parentValue} onChange={onChange}>
+    <option>none</option>
+    <option value="1">Moscow</option>
+    <option value="2">Saint-P</option>
+    <option value="3">Yaroslavl</option>
+  </select>
+}
+
+export const ControlledInputWithFixedValue = () => <input value={'fixed value'}/>
